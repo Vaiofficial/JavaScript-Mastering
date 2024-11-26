6ba3adcae63300fe62eabf01c806e3fcd53b979a
P Vaibhav <pvaibhavofficial@gmail.com>
2024-11-26 16:19:51 +0530

    added lec on variables and datatypes

---
+++ 01 Basics/01variables.js
@@ -0,0 +1,10 @@
+// "use strict";
+const accoundId = 12345;
+let accountEmail = "vaibhav@gmail.com";
+var accountPassword = "passwo"
+accountCity = "raipur"
+
+// accoundId = 2;
+
+
+console.table([accoundId, accountEmail, accountPassword, accountCity])
---
+++ 01 Basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"
+
+// alert(3+4) // we are using nodejs, not browser
+
+console.log(2+3);
+console.log(3+4); //code readabilty should be high
+
+let name = "vaibhav"; //string data type
+let age = 12; // integer data type
+let isLoggedIn = false; //bool data type
+
+console.log("the type of name variable is: ",typeof name)
+
+console.log(typeof null) //OBJECT
+
+console.log(typeof undefined) //UNDEFINED
